76c3f01c01637554984880f4064f23c2824bb952
wangxun <1013296326@qq.com>
2024-08-01 07:19:41 +0800

    up to view with db

--- viper/views/workflow.py
+++ viper/views/workflow.py
@@ -4,10 +4,10 @@ from touch.models.workflow import WorkflowModel
 
 
 async def create_workflow(request):
-    # data = await request.json()
-    # params = {
-    #     'name': data['workflow_name'],
-    #     'desc': data['desc']
-    # }
-    # workflow_id = await WorkflowModel().add_workflow(params)
-    return JSONResponse({'workflow_id': 'qaz'})
+    data = await request.json()
+    params = {
+        'name': data['workflow_name'],
+        'desc': data['desc']
+    }
+    workflow_id = await WorkflowModel().add_workflow(params)
+    return JSONResponse({'workflow_id': workflow_id})
