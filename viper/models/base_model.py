7c3331568e8e69841dede4f9888176b7f6e95261
wangxun <1013296326@qq.com>
2024-12-13 18:51:15 +0800

    up 2 dbmodels

--- viper/models/base_model.py
+++ viper/models/base_model.py
@@ -9,24 +9,19 @@ class BaseModel:
 
     async def conn(self):
         if not self.cursor:
-            if not self._conn:
-                self._conn = await db.acquire()  # 获取连接
+            self._conn = await db.acquire()  # 获取连接
             self.cursor = await self._conn.cursor()  # 获取游标
 
     async def rollback(self):
-        if not self._conn:
-            raise ValueError('connection is not acquired')
         await self._conn.rollback()
 
     async def close(self):
         try:
+            if self.cursor:
+                await self.cursor.execute('UNLOCK TABLES;')
+                await self.cursor.close()  # 关闭游标
             if self._conn:
-                if self.cursor:
-                    await self.cursor.execute('UNLOCK TABLES;')
-                    await self.cursor.close()  # 关闭游标
                 await db.release(self._conn)  # 连接放回连接池
-        except Exception as exc:
-            raise Exception(f'db close failed:{exc}')
         finally:
             self.cursor = None
             self._conn = None
@@ -34,22 +29,22 @@ class BaseModel:
     async def commit(self):
         try:
             await self._conn.commit()
-        except Exception as exc:
+        except Exception:
             await self.rollback()
-            raise Exception(f'db commit failed:{exc}')
+            raise
 
     async def execute(self, sql_str, values=None):
         try:
             await self.cursor.execute(sql_str, values)
-        except Exception as exc:
+        except Exception:
             await self.rollback()
             await self.close()
-            raise Exception(f'db execute failed:{exc}')
+            raise
 
     async def executemany(self, sql_str, values=None):
         try:
             await self.cursor.executemany(sql_str, values)    # values is [tuple1, tuple2]
-        except Exception as exc:
+        except Exception:
             await self.rollback()
             await self.close()
-            raise Exception(f'db executemany failed:{exc}')
+            raise
