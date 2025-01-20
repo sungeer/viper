1f2c629b451ce900bd4071ba9b4aebedb0275b78
wangxun <1013296326@qq.com>
2025-01-20 08:28:00 +0800

    up to exception handler

--- viper/core/errors.py
+++ viper/core/errors.py
@@ -1,17 +1,28 @@
+from starlette.requests import Request
 from starlette.exceptions import HTTPException
 
 from viper.utils.log_util import logger
 from viper.utils.resp_util import jsonify_exc
 from viper.utils.errors import ValidationError
 
+
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    logger.opt(exception=True).warning(exc)
+    return jsonify_exc(422, exc.args)
+
+
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.opt(exception=True).warning(exc)
+    return jsonify_exc(exc.status_code, exc.detail)
+
+
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.exception(exc)
+    return jsonify_exc(500)
+
+
 register_errors = {
-    ValidationError: lambda request, exc: (
-        logger.opt(exception=True).warning(exc) or jsonify_exc(422, exc.args)
-    ),
-    HTTPException: lambda request, exc: (
-        logger.opt(exception=True).warning(exc) or jsonify_exc(exc.status_code, exc.detail)
-    ),
-    Exception: lambda request, exc: (
-        logger.exception(exc) or jsonify_exc(500)
-    ),
+    ValidationError: validation_exception_handler,
+    HTTPException: http_exception_handler,
+    Exception: global_exception_handler,
 }
