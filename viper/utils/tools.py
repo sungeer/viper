5562c712a409c93bb9909a848c773e48bbc4ebdf
wangxun <1013296326@qq.com>
2025-01-25 19:37:39 +0800

    up uuid string util

--- viper/utils/tools.py
+++ viper/utils/tools.py
@@ -7,7 +7,7 @@ def generate_uuid():
     random_uuid = uuid.uuid4()
     uuid_str = str(random_uuid)
     md5_hash = hashlib.md5()
-    md5_hash.update(uuid_str.encode())  # type: ignore
+    md5_hash.update(uuid_str.encode())
     return md5_hash.hexdigest()  # result is lower
 
 
