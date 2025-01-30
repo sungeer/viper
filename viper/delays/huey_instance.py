0999096a4544bdf17b817f2628f1c4b87f7ff2c5
wangxun <1013296326@qq.com>
2025-01-30 14:46:42 +0800

    edit app name on huey

--- viper/delays/huey_instance.py
+++ viper/delays/huey_instance.py
@@ -11,6 +11,8 @@ redis_pool = ConnectionPool(
     db=0
 )
 
-huey = RedisHuey(settings.CONF.get_conf('APP', 'NAME'), connection_pool=redis_pool, blocking=True)
+app_name = settings.CONF.get_conf('APP', 'NAME')
+
+huey = RedisHuey(app_name, connection_pool=redis_pool, blocking=True)
 
 from viper.delays import schedules  # noqa 在消费者 启动时 加载 定时任务
